6d8aafb61a134edef643e706392633a62095265c
Viselda Beqiraj <viselda.beqiraj@jkmoving.com>
2022-12-01 19:02:55 +0100

    change jobdetailsdto

--- MFC-VoxMe.Core/Dtos/Jobs/JobDetailsDto.cs
+++ MFC-VoxMe.Core/Dtos/Jobs/JobDetailsDto.cs
@@ -1,4 +1,6 @@
-﻿namespace MFC_VoxMe_API.Dtos.Jobs
+﻿using static MFC_VoxMe_API.Dtos.Jobs.CreateJobDto;
+
+namespace MFC_VoxMe_API.Dtos.Jobs
 {
     public class JobDetailsDto
     {
@@ -6,53 +8,79 @@
             public string uniqueId { get; set; }
             public string jobType { get; set; }
             public string jobStatus { get; set; }
-            public List<object> documents { get; set; }
+            public List<Document> documents { get; set; }
             public JobInventory jobInventory { get; set; }
         
+
         public class JobInventory
         {
-            public int piecesNr { get; set; }
             public int firstLabelNr { get; set; }
-            public int lastLabelNr { get; set; }
-            public int volume { get; set; }
-            public int weight { get; set; }
             public int grossVolume { get; set; }
             public int grossWeight { get; set; }
+            public int lastLabelNr { get; set; }
+            public int piecesNr { get; set; }
             public double value { get; set; }
+            public int volume { get; set; }
+            public int weight { get; set; }
             public string labelColor { get; set; }
             public string firstTag { get; set; }
             public List<Room> rooms { get; set; }
             public List<Packer> packers { get; set; }
-            public List<object> loadingUnits { get; set; }
+            public List<LoadingUnit> loadingUnits { get; set; }
             public List<Piece> pieces { get; set; }
+        }
+
+        public class LoadingUnit
+        {
+            public double extHeight { get; set; }
+            public double extLength { get; set; }
+            public double extWidth { get; set; }
+            public double grossVolume { get; set; }
+            public double grossWeight { get; set; }
+            public double netHeight { get; set; }
+            public double netLength { get; set; }
+            public double netVolume { get; set; }
+            public double netWeight { get; set; }
+            public double netWidth { get; set; }
+            public string photos { get; set; }
+            public string serialNumber { get; set; }
+            public string uniqueId { get; set; }
+            public string unitType { get; set; }
+            public int labelNr { get; set; }
+            public string sealNumber { get; set; }
+            public string warehouseLocation { get; set; }
+            public string storageUnit { get; set; }
+            public DateTime dateIn { get; set; }
+            public DateTime dateOut { get; set; }
         }
 
         public class Packer
         {
-            public string packer { get; set; }
             public bool isForeman { get; set; }
+            public string packer { get; set; }
         }
 
         public class Piece
         {
-            public string labelNr { get; set; }
-            public string tag { get; set; }
             public string barcode { get; set; }
-            public string packerName { get; set; }
-            public string roomName { get; set; }
-            public bool pbo { get; set; }
-            public string packageType { get; set; }
-            public int packageUnitCost { get; set; }
+            public double height { get; set; }
+            public string labelNr { get; set; }
+            public double length { get; set; }
             public int packageQty { get; set; }
-            public object loadUnitUniqueId { get; set; }
+            public string packageType { get; set; }
+            public double packageUnitCost { get; set; }
+            public string packerName { get; set; }
+            public bool pbo { get; set; }
+            public string roomName { get; set; }
+            public string tag { get; set; }
             public bool @void { get; set; }
-            public int width { get; set; }
-            public int height { get; set; }
-            public int length { get; set; }
-            public int volume { get; set; }
-            public int weight { get; set; }
-            public List<object> items { get; set; }
+            public double volume { get; set; }
+            public double weight { get; set; }
+            public double width { get; set; }
+            public string loadUnitUniqueId { get; set; }
+            public List<Item> items { get; set; }
         }
+       
 
         public class Room
         {
